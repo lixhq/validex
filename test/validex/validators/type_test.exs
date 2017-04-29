200144653a56739cd5ca43753fab02939a4e9deb
Simon Stender Boisen <simon@skov-boisen.dk>
2017-04-29 15:21:17 +0200

    Add type tests

--- test/validex/validators/type_test.exs
+++ test/validex/validators/type_test.exs
@@ -1,14 +1,32 @@
 defmodule TypeTest do
   use ExUnit.Case, async: true
 
-  def do_type_test(type, value, :ok) do
-    assert [{:ok, :prop, :presence}, {:ok, :prop, :type}
-    ] = Validex.verify(%{ prop: value}, [prop: [type: type]])
+  @types [:string, :integer, :float, :number, :atom, :list, :map]
+  @values ["hello", 5, 5.6, 5.6, :bla, [], %{}]
+
+  @valid_cases Enum.zip(@types, @values) ++ Enum.map(@values, &{:any, &1}) ++ [{:number, 5}]
+
+  @invalid_cases Enum.uniq(
+    for t <- @types, v <- @values, do: {t, v}
+  ) -- @valid_cases
+
+
+  test "valid cases all checks out" do
+
+    Enum.each(@valid_cases, fn {t, v} ->
+      assert [
+        {:ok, :prop, :presence}, {:ok, :prop, :type}
+      ] = Validex.verify(%{ prop: v }, [prop: [presence: true, type: t]])
+    end)
+
   end
 
-  def do_type_test(type, value, :error) do
-    assert [{:ok, :prop, :presence}, {:error, :prop, :type, _}
-    ] = Validex.verify(%{ prop: value}, Keyword.new([{:prop, type}]))
+  test "invalid cases all checks out" do
+    Enum.each(@invalid_cases, fn {t, v} ->
+      assert [
+        {:ok, :prop, :presence}, {:error, :prop, :type, _}
+      ] = Validex.verify(%{ prop: v }, [prop: [presence: true, type: t]])
+    end)
   end
 end
 
